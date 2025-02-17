9d4c87b135bedff7ec8891c443bdb3f4517d3a1d
Renz Ryan Santillana <137261297+rsantillana@users.noreply.github.com>
2025-02-16 18:59:46 -0800

    Update Account.java

--- Account.java
+++ Account.java
@@ -18,10 +18,46 @@ public class Account
 	}
 	
 	//ToDo Display Rate
+	public void displayRate()
+	{
+		string output = "The interest rate is %.2f%%;
+		output = String.format(ourput, rate * 100);
+		System.out.println(output);
+	}
 	
 	//ToDo Deposit Money
+	public boolean deposit(double amount)
+	{
+		if (amount > 0)
+		{
+			balance += amount;
+			return true;
+		}
+		return false;
+	}
 	
 	//ToDo Withdraw Money
+	public boolean withdraw(double amount)
+	{
+		if (amount > 0 && amounr <= balance)
+		{
+			balance -= amount;
+			return true;
+		}
+		return false;
+	}
 	
 	//ToDo Project Interest
+	public boolean accrueInterest(int months)
+	{
+		if (months > 0)
+		{
+			for (int i = 0; i < months; i++)
+				{
+					balance += (balance * rate)/12;
+				}
+			return true;
+		}
+		return false;
+	}
 }
